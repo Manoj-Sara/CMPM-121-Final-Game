dd053cf2f49ab65fc56cebe64f221711f27f9396
ManojSara <msara@ucsc.edu>
2022-12-07 15:29:34 -0800

    stun working

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -10,22 +10,22 @@ public class EnemyController : MonoBehaviour
 {
     // stats
     public float baseChaseSpeed = 25f;
-    public float baseSearchSpeed = 5f;
     public float detectDist = 20f;
     public int chaseLevel = 20;
+    public bool stunned = false;
 
     private float dist;
     // timers
     public bool chasing = false;
     private float chaseDuration = 10f;
     private float chaseTimer = 0f;
+    private float stunTime = 5f;
 
     public bool chaseCooldown = false;
     private float chaseCooldownDuration = 5f;
     private float chaseCooldownTimer = 0f;
 
     private Vector3 dir;
-    private bool gameOver = false;
 
     // obj vars
     public Transform player;
@@ -43,6 +43,11 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (stunned) {
+            rb.velocity = Vector3.zero;
+            return;
+        }
+
         dist = Vector3.Distance(transform.position, player.position);
         dir = (player.position - transform.position).normalized;
         Vector3 pos = ProjectPointOnPlane(transform.up, transform.position, player.position);
@@ -87,9 +92,14 @@ public class EnemyController : MonoBehaviour
         }
     }
 
-    // private void SearchPlayer() {
-    //     print("searching for player");
-    //     float searchSpeed = baseSearchSpeed*(1f+(chaseLevel*0.1f));
-    //     rb.velocity = transform.forward*searchSpeed;
-    // }
+    public void Stun() {
+        stunned = true;
+        print("STUN!");
+        StartCoroutine(StunDelay());
+    }
+
+    private IEnumerator StunDelay() {
+        yield return new WaitForSeconds(stunTime);
+        stunned = false;
+    }
 }
